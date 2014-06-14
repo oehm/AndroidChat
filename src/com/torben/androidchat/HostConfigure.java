58781db634f90cb20db30705c122db628a17874f
Tobias Hoffmann <thoffmann.mmt-b2012@fh-salzburg.ac.at>
2014-06-14 18:19:34 +0200

    some ui rework and some bugs fixed

--- src/com/torben/androidchat/HostConfigure.java
+++ src/com/torben/androidchat/HostConfigure.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.TextView;
 import android.widget.ToggleButton;
@@ -12,6 +13,7 @@ import android.widget.ToggleButton;
 public class HostConfigure extends Activity {
 
 	private ToggleButton toggleSockets_;
+	private ToggleButton toggleRPC_;
 	private TextView textIp_;
 	private TextView textPort_;
 	
@@ -22,16 +24,9 @@ public class HostConfigure extends Activity {
 		// Show the Up button in the action bar.
 		getActionBar().setDisplayHomeAsUpEnabled(true);
 		
-		//sockets:
-		toggleSockets_ = (ToggleButton) findViewById(R.id.toggle_host_socket);
-		toggleSockets_.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-		    @Override
-		    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-		        // Save the state here
-		    	Host.Instance().setSocketsState(isChecked);
-		    }
-		});
-		toggleSockets_.setChecked(Host.Instance().getSocketState());
+		setupSockets();
+		setupRPC();
+		
 		
 		textIp_ = (TextView) findViewById(R.id.text_host_ip);
 		textPort_ = (TextView) findViewById(R.id.text_host_port);
@@ -65,5 +60,44 @@ public class HostConfigure extends Activity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+	
+	private void setupSockets(){
+		toggleSockets_ = (ToggleButton) findViewById(R.id.toggle_host_socket);
+		toggleSockets_.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+		    @Override
+		    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+		        // Save the state here
+		    	Host.Instance().setSocketsState(isChecked);
+		    }
+		});
+		toggleSockets_.setChecked(Host.Instance().getSocketState());
+	}
+	
+	private void setupRPC(){
+		toggleRPC_ = (ToggleButton) findViewById(R.id.toggle_host_rpc);
+		toggleRPC_.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+		    @Override
+		    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+		        // Save the state here
+		    	Host.Instance().setRPCState(isChecked);
+		    }
+		});
+		toggleRPC_.setChecked(Host.Instance().getRPCState());
+	}
+	
+	/* Called when the user clicks the refresh sockets button.*/
+    public void refreshSockets(View view){
+    	if(Host.Instance().ip!=null)
+			textIp_.setText(Host.Instance().ip);
+    	else
+    		textIp_.setText(R.string.text_host_ip);
+		
+		if(Host.Instance().port!=0)
+			textPort_.setText(Host.Instance().port+"");
+    	else
+    		textPort_.setText(R.string.text_host_port);
+		
+		toggleSockets_.setChecked(Host.Instance().getSocketState());
 	}
 }
